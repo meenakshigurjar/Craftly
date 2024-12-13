36fa0c475e2030393f7f4196e7bd3b94d98e0657
Meenakshi <meenakshigurjar20@gmail.com>
2024-12-13 14:23:08 +0530

    Footer updated

--- src/components/demo/DemoAsset.jsx
+++ src/components/demo/DemoAsset.jsx
@@ -3,28 +3,90 @@ import { Button } from "@/components/ui/button";
 import union from "/Images/Union.png";
 
 export default function DemoAsset() {
+  const footerData = [
+    {
+      title: "Use Cases",
+      items: [
+        "Generate Images via API",
+        "Watermark Videos via API",
+        "Generate PDFs via API",
+        "Generate Images with Zapier",
+        "Watermark Videos with Zapier",
+        "Generate PDFs with Zapier",
+        "More Use Cases",
+      ],
+    },
+    {
+      title: "Products",
+      items: [
+        "Image Generation API",
+        "Multi Image Generation API",
+        "Video Generation API",
+        "PDF Generation API",
+        "Template Library",
+        "Template Library",
+      ],
+    },
+    {
+      title: "Integration",
+      items: [
+        "Airtable Integration",
+        "Zapier Integration",
+        "Integromat Integration",
+        "Forms",
+        "URLS",
+        "WordPress",
+      ],
+    },
+    {
+      title: "Demos",
+      items: [
+        "Multi Image Demo",
+        "AI Face Detect Demo",
+        "Twitter to Instagram",
+        "Real Estate Demo",
+        "Github Social",
+        "Open Graph",
+        "Smart Crop Demo",
+        "Online Certificate Maker",
+        "Online Wedding Invite Maker",
+        "Online Event ID Card Maker",
+        "Online Photo Collage Maker",
+        "Online Invoice Maker",
+      ],
+    },
+    {
+      title: "Docs & Guides",
+      items: [
+        "Help Articles",
+        "Blog",
+        "eBooks",
+        "API Quick Start",
+        "API Reference",
+      ],
+    },
+  ];
   return (
-    <div>
+    <div className="h-fit">
       {/* Hero Section */}
       <div
-  className="relative w-full h-[100vh] bg-cover bg-center"
+  className="relative w-full h-[100vh] md:h-[80vh] sm:h-[20vh] bg-cover bg-center"
   style={{ backgroundImage: `url(${union})` }}
 >
   {/* Main Content Wrapper */}
-  <div className="absolute top-[200px] inset-x-0 flex flex-col items-center text-center px-4">
+  <div className="absolute top-[15%] md:top-[25%] inset-x-0 flex flex-col items-center text-center px-4 space-y-8">
     {/* Heading */}
-    <h1 className="text-black font-Archivo text-center w-full max-w-[90%] 
-      text-[20px] sm:text-[24px] md:text-[30px] lg:text-[50px] leading-tight">
+    <h1 className="text-2xl md:text-4xl lg:text-5xl font-Archivo text-black">
       Slow asset production is killing your growth. Unlock your true potential now.
     </h1>
 
     {/* Button Wrapper */}
-    <div className="absolute w-[90%] sm:w-[300px] md:w-[400px] lg:w-[400px] 
-      h-[60px] sm:h-[70px] md:h-[90px] lg:h-[98px] 
-      top-[200px] sm:top-[150px] lg:top-[300px] flex justify-center">
+    <div className=" w-[90%] sm:w-[200px] md:w-[300px] lg:w-[300px] 
+      h-[60px] sm:h-[70px] md:h-[80px] lg:h-[90px] 
+       flex justify-center">
       <Button
         className="text-white flex items-center justify-center w-full h-full 
-          text-[16px] sm:text-[20px] md:text-[24px] lg:text-[34px] mx-auto"
+          text-[16px] sm:text-[20px] md:text-[24px] lg:text-[30px] mx-auto"
         style={{
           fontFamily: "Archivo",
           borderBottom: "4px solid black",
@@ -40,143 +102,37 @@ export default function DemoAsset() {
 
 
       {/* Footer */}
-      <div className="flex flex-col items-center justify-center bg-black p-8 mb-[100px]">
-        <div className="flex flex-wrap justify-evenly gap-y-8 gap-x-4 mb-[100px]">
-          <div className="flex flex-col h-fit md:h-[496px]">
-            <div className="text-white text-[30px]/[79px] font-Archivo ">
-              Use Cases
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Generate Images via API
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Watermark Videos via API
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Generate PDFs via API
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Generate Images with Zapier
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Watermark Videos with Zapier
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Generate PDFs with Zapier
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              More Use Cases
-            </div>
-          </div>
-          <div className="flex flex-col h-fit md:h-[437px]">
-            <div className="text-white text-[30px]/[79px] font-Archivo ">
-              Products
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Image Generation API
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Multi Image Generation API
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Video Generation API
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              PDF Generation API
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Template Library
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Template Library
-            </div>
-          </div>
-          <div className="flex flex-col  h-fit md:h-[437px]">
+
+    <div className="flex flex-col items-center justify-center bg-black p-8 mb-[100px]">
+      <div className="flex flex-wrap justify-evenly gap-y-8 gap-x-4 mb-[100px]">
+        {footerData.map((section, index) => (
+          <div
+            key={index}
+            className={`flex flex-col h-fit ${
+              section.title === "Demos"
+                ? "md:h-[708px]"
+                : section.title === "Use Cases"
+                ? "md:h-[496px]"
+                : section.title === "Products" || section.title === "Integration"
+                ? "md:h-[437px]"
+                : "md:h-[378px]"
+            }`}
+          >
             <div className="text-white text-[30px]/[79px] font-Archivo">
-              Integration
+              {section.title}
             </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Airtable Integration
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Zapier Integration
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Integromat Integration
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Forms
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              URLS
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              WordPress
-            </div>
+            {section.items.map((item, idx) => (
+              <div
+                key={idx}
+                className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]"
+              >
+                {item}
+              </div>
+            ))}
           </div>
-          <div className="flex flex-col  h-fit md:h-[708px]">
-            <div className="text-white text-[30px]/[79px] font-Archivo">
-              Demos
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Multi Image Demo
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Al Face Detect Demo
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Twitter to Instagram
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Real Estate Demo
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Github Social
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Open Graph
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Smart Crop Demo
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Online Certificate Maker
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Online Wedding Invite Maker
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Online Event ID Card Maker
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Online Photo Collage Maker
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Online Invoice Maker
-            </div>
-          </div>
-          <div className="flex flex-col  h-fit md:h-[378px]">
-            <div className="text-white text-[30px]/[79px] font-Archivo">
-              Docs & Guides
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Help Articles
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              Blog
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              eBooks
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              API Quick Start
-            </div>
-            <div className="text-[#FFFFFF61] font-medium font-Inter text-[20px]/[59px]">
-              API Reference
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
+    </div>
     </div>
   );
 }
